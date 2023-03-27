708552df372d6c5bb7d398c1439f594c9da0a7b9
riskiferdian <riskiferdian9@gmail.com>
2023-03-27 09:10:50 +0700

    template

--- src/test/java/starter/CookitAlta/Utils/Constant.java
+++ src/test/java/starter/CookitAlta/Utils/Constant.java
@@ -1,7 +1,7 @@
 package starter.CookitAlta.Utils;
 
 public class Constant {
-    public static String BASE_URL = "https://dummyjson.com/";
+    public static String BASE_URL = "https://cookit.my-extravaganza.site/";
     public static final String DIR = System.getProperty("user.dir");
     public static String JSON_REQUEST = DIR+"/src/test/resources/JSON/JSONRequest/";
     public static String JSON_SCHEMA = DIR+"/src/test/resources/JSON/JSONSchema/";
