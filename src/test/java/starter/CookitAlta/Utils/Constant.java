ba79e4aaed671b946d48b10d36e7aa0d207f4758
abdshidiq <arshidiq182@gmail.com>
2023-03-31 16:11:10 +0700

    Users

--- src/test/java/starter/CookitAlta/Utils/Constant.java
+++ src/test/java/starter/CookitAlta/Utils/Constant.java
@@ -6,5 +6,5 @@ public class Constant {
     public static String JSON_REQUEST = DIR+"/src/test/resources/JSON/JSONRequest/";
     public static String JSON_SCHEMA = DIR+"/src/test/resources/JSON/JSONSchema/";
     public static String BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2ODMzOTczMDksInJvbGUiOiJVc2VyIiwidXNlcklkIjoyM30.HYDMBFDX-QEtaCCT1Zb6hSshB9VNrjJ__NnOqkDHZuY";
-    public static String INVALID_TOKEN = "eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2ODMyMjEzMDQsInJvbGUiOiJVc2VyIiwidXNlcklkIjoyM30.mUQEMFOuDjqN_WoK3PPBPoGl7D3zfFFgVshBnf8LAEY";
+    public static String INVALID_TOKEN = "";
 }
